47e127c4756ed5050df6eca831e5553d73f5ea8d
hodolee246 <hodolee246@naver.com>
2025-05-11 22:19:02 +0900

    chore: 멀티모듈 프로젝트 초기 설정

---
+++ src/test/java/com/hodolee/example/fortunetelling/FortuneTellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodolee.example.fortunetelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneTellingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
