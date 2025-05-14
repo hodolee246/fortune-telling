6e0ebde3d3a2c2fd056395bb70b48e3511bff274
hodolee246 <hodolee246@naver.com>
2025-05-14 22:59:36 +0900

    feat: 의존성 설정 및 파일 인코딩 UTF-8로 변경

--- api/src/main/java/com/hodolee/example/api/fortune/FortuneController.java
+++ api/src/main/java/com/hodolee/example/api/fortune/FortuneController.java
@@ -1,7 +1,15 @@
 package com.hodolee.example.api.fortune;
 
+import com.hodolee.example.core.fortune.FortuneService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FortuneController {
+
+    private final FortuneService fortService;
+
+    public FortuneController(FortuneService fortService) {
+        this.fortService = fortService;
+    }
+
 }
