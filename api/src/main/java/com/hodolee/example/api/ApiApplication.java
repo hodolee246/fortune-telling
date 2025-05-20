168bdc4b83e119257d835ea043abb0aa5fa67f7e
hodolee246 <hodolee246@naver.com>
2025-05-20 22:58:55 +0900

    modify: 컴포넌트 스캔 패키지 최상단으로 변경

--- api/src/main/java/com/hodolee/example/api/ApiApplication.java
+++ api/src/main/java/com/hodolee/example/api/ApiApplication.java
@@ -6,15 +6,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {
-		"com.hodolee.example.api",
-		"com.hodolee.example.core",
-		"com.hodolee.example.domain",
-		"com.hodolee.example.infra"
-})
-@EnableJpaRepositories(basePackages = "com.hodolee.example.domain.fortune.domain")
-@EntityScan(basePackages = "com.hodolee.example.domain.fortune.domain")
+@SpringBootApplication(scanBasePackages = "com.hodolee.example")
+@EntityScan(basePackages = "com.hodolee.example")
+@EnableJpaRepositories(basePackages = "com.hodolee.example")
 public class ApiApplication {
 
 	public static void main(String[] args) {
