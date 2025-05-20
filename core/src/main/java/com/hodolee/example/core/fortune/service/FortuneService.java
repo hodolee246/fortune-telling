94ccbed8d1cb8bce5d2eb9d10835c873937b1af5
hodolee246 <hodolee246@naver.com>
2025-05-20 22:20:27 +0900

    feat: h2 driver 추가

--- core/src/main/java/com/hodolee/example/core/fortune/service/FortuneService.java
+++ core/src/main/java/com/hodolee/example/core/fortune/service/FortuneService.java
@@ -27,13 +27,13 @@ public class FortuneService {
     public FortuneResponse getFortune(String name, String birthDate) {
         // 네이버 운세 크롤링 반환
         String fortuneText = naverFortuneClient.getFortune(birthDate);
-        fortuneRepository.save(Fortune.builder()
-                .name(name)
-                .birthDate(birthDate)
-                .fortuneText(fortuneText)
-                .build());
+//        fortuneRepository.save(Fortune.builder()
+//                .name(name)
+//                .birthDate(birthDate)
+//                .fortuneText(fortuneText)
+//                .build());
         // url
-        String generatedFortuneUrl = urlGenerator.generateEncodeUrl(fortuneText);
+        String generatedFortuneUrl = urlGenerator.generateEncodeUrl(1L);
 
         return new FortuneResponse(name, birthDate, generatedFortuneUrl);
     }
