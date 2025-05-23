35c91b154e5d9d7231092fe51d1e780112d9ef87
hodolee246 <hodolee246@naver.com>
2025-05-24 04:12:26 +0900

    modify: 운세 생성 시 분산락 적용하여 한번에 너무 많은 요청에 대한 처리 추가

--- core/src/main/java/com/hodolee/example/core/fortune/service/FortuneService.java
+++ core/src/main/java/com/hodolee/example/core/fortune/service/FortuneService.java
@@ -6,54 +6,89 @@ import com.hodolee.example.domain.fortune.domain.FortuneRepository;
 import com.hodolee.example.infra.fortune.external.NaverFortuneClient;
 import com.hodolee.example.infra.fortune.url.UrlGenerator;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Slf4j
 public class FortuneService {
 
     private final NaverFortuneClient naverFortuneClient;
     private final UrlGenerator urlGenerator;
     private final FortuneRepository fortuneRepository;
-
-    public FortuneService(NaverFortuneClient naverFortuneClient, UrlGenerator urlGenerator, FortuneRepository fortuneRepository) {
-        this.naverFortuneClient = naverFortuneClient;
-        this.urlGenerator = urlGenerator;
-        this.fortuneRepository = fortuneRepository;
-    }
+    private final RedissonClient redissonClient;
 
     @CircuitBreaker(name = "fortuneService", fallbackMethod = "getDefaultFortuneUrl")
     @Transactional
     public String getFortuneUrl(String name, String birthDate) {
-        // 네이버 운세 크롤링 반환
-        String fortuneText = naverFortuneClient.getFortune(birthDate);
-        Fortune savedFortune = fortuneRepository.save(Fortune.builder()
-                .name(name)
-                .birthDate(birthDate)
-                .fortuneText(fortuneText)
-                .build());
-        // url
-        String generatedFortuneUrl = urlGenerator.generateEncodeUrl(savedFortune.getIdx());
+        // redis 분산락
+        String lockKey = "fortune:" + name + ":" + birthDate;
+        RLock lock = redissonClient.getLock(lockKey);
 
-        return generatedFortuneUrl;
+        boolean acquired = false;
+        try {
+            acquired = lock.tryLock(3, 10, TimeUnit.SECONDS);
+
+            if (!acquired) {
+                throw new IllegalStateException("잠시 후 다시 시도해주세요.");
+            }
+
+            // 네이버 운세 크롤링 반환
+            String fortuneText = naverFortuneClient.getFortune(birthDate);
+
+            // url
+            return saveFortuneAndReturnUrl(Fortune.builder()
+                    .name(name)
+                    .birthDate(birthDate)
+                    .fortuneText(fortuneText)
+                    .build());
+        } catch (InterruptedException e) {
+            throw new IllegalArgumentException("락 획득 중 예외 발생 : " + e.getMessage());
+        } finally {
+            if (acquired && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
     }
 
     private String getDefaultFortuneUrl(String name, String birthDate, Throwable t) {
         log.info("서킷브레이크 활성화 : {}", t.getMessage());
-        String defaultFortune = "하늘을 조심하세요.";
-        // 기본 운세 저장
-        Fortune savedFortune = fortuneRepository.save(Fortune.builder()
-                .name(name)
-                .birthDate(birthDate)
-                .fortuneText(defaultFortune)
-                .build());
-        // url
-        String generatedFortuneUrl = urlGenerator.generateEncodeUrl(savedFortune.getIdx());
+        // redis 분산락
+        String lockKey = "fortune:" + name + ":" + birthDate;
+        RLock lock = redissonClient.getLock(lockKey);
 
-        return generatedFortuneUrl;
+        boolean acquired = false;
+
+        try {
+            acquired = lock.tryLock(3, 10, TimeUnit.SECONDS);
+
+            if (!acquired) {
+                throw new IllegalStateException("잠시 후 다시 시도해주세요.");
+            }
+
+            String defaultFortune = "하늘을 조심하세요.";
+
+            // url
+            return saveFortuneAndReturnUrl(Fortune.builder()
+                    .name(name)
+                    .birthDate(birthDate)
+                    .fortuneText(defaultFortune)
+                    .build());
+        } catch (InterruptedException e) {
+            throw new IllegalArgumentException("락 획득 중 예외 발생 : " + e.getMessage());
+        } finally {
+            if (acquired && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
     }
 
     public FortuneResponse getFortune(String encryptIdx) {
@@ -65,6 +100,12 @@ public class FortuneService {
         getFortune.incrementViewCount();
 
         return new FortuneResponse(getFortune.getName(), getFortune.getBirthDate(), getFortune.getFortuneText());
+    }
+
+    private String saveFortuneAndReturnUrl(Fortune fortune) {
+        Fortune savedFortune = fortuneRepository.save(fortune);
+
+        return urlGenerator.generateEncodeUrl(savedFortune.getIdx());
     }
 
 }
